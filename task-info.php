069d5a016e2a5146914a47bf4f0f249eb963d41b
X10N <116271291+escalona-mj@users.noreply.github.com>
2024-04-03 16:53:54 +0800

    added design to sidebar

--- task-info.php
+++ task-info.php
@@ -25,197 +25,221 @@ if(isset($_GET['delete_task'])){
 if(isset($_POST['add_task_post'])){
     $obj_admin->add_new_task($_POST);
 }
-
-$page_name="Task_Info";
-include("include/sidebar.php");
-// include('ems_header.php');
-
-
 ?>
 
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Employee Task Management System</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <!-- Modal -->
-  <div class="modal fade" id="myModal" role="dialog">
-    <div class="modal-dialog add-category-modal">
-    
-      <!-- Modal content-->
-      <div class="modal-content">
-        <div class="modal-header">
-          <button type="button" class="close" data-dismiss="modal">&times;</button>
-          <h2 class="modal-title text-center">Assign New Task</h2>
-        </div>
-        <div class="modal-body">
-          <div class="row">
-            <div class="col-md-12">
-              <form role="form" action="" method="post" autocomplete="off">
-                <div class="form-horizontal">
-                  <div class="form-group">
-                    <label class="control-label col-sm-5">Task Title</label>
-                    <div class="col-sm-7">
-                      <input type="text" placeholder="Task Title" id="task_title" name="task_title" list="expense" class="form-control" id="default" required>
-                    </div>
-                  </div>
-                  <div class="form-group">
-                    <label class="control-label col-sm-5">Task Description</label>
-                    <div class="col-sm-7">
-                      <textarea name="task_description" id="task_description" placeholder="Text Deskcription" class="form-control" rows="5" cols="5"></textarea>
-                    </div>
-                  </div>
-                  <div class="form-group">
-                    <label class="control-label col-sm-5">Start Time</label>
-                    <div class="col-sm-7">
-                      <input type="text" name="t_start_time" id="t_start_time" class="form-control">
-                    </div>
-                  </div>
-                  <div class="form-group">
-                    <label class="control-label col-sm-5">End Time</label>
-                    <div class="col-sm-7">
-                      <input type="text" name="t_end_time" id="t_end_time" class="form-control">
-                    </div>
-                  </div>
-                  <div class="form-group">
-                    <label class="control-label col-sm-5">Assign To</label>
-                    <div class="col-sm-7">
-                      <?php 
-                        $sql = "SELECT user_id, fullname FROM tbl_admin WHERE user_role = 2";
-                        $info = $obj_admin->manage_all_info($sql);   
-                      ?>
-                      <select class="form-control" name="assign_to" id="aassign_to" required>
-                        <option value="">Select Employee...</option>
+  <!-- bootstrap 🤮 -->
+  <!-- <link rel="stylesheet" href="assets/css/bootstrap.min.css"> -->
+  <!-- <link rel="stylesheet" href="assets/css/bootstrap.theme.min.css"> -->
+  <link rel="stylesheet" href="assets/bootstrap-datepicker/css/datepicker.css">
+  <link rel="stylesheet" href="assets/bootstrap-datepicker/css/datepicker-custom.css">
 
-                        <?php while($row = $info->fetch(PDO::FETCH_ASSOC)){ ?>
-                        <option value="<?php echo $row['user_id']; ?>"><?php echo $row['fullname']; ?></option>
-                        <?php } ?>
-                      </select>
-                    </div>
-                   
-                  </div>
-                  <div class="form-group">
-                  </div>
-                  <div class="form-group">
-                    <div class="col-sm-offset-3 col-sm-3">
-                      <button type="submit" name="add_task_post" class="btn btn-success-custom">Assign Task</button>
-                    </div>
-                    <div class="col-sm-3">
-                      <button type="submit" class="btn btn-danger-custom" data-dismiss="modal">Cancel</button>
-                    </div>
-                  </div>
-                </div>
-              </form> 
-            </div>
-          </div>
+  <!-- <link rel="stylesheet" href="assets/css/custom.css"> -->
+  <link rel="stylesheet" href="assets/css/style.css">
 
-        </div>
-      </div>
-    </div>
-  </div>
+  <script src="assets/js/jquery.min.js"></script>
+  <script src="assets/js/bootstrap.min.js"></script>
+  <script src="assets/js/custom.js"></script>
+  <script src="assets/bootstrap-datepicker/js/bootstrap-datepicker.js"></script>
+  <script src="assets/bootstrap-datepicker/js/datepicker-custom.js"></script>
 
-
-
-
-
-    <div class="row">
-      <div class="col-md-12">
-        <div class="well well-custom">
-          <div class="gap"></div>
-          <div class="row">
-            <div class="col-md-8">
-              <div class="btn-group">
-                <?php if($user_role == 1){ ?>
-                <div class="btn-group">
-                  <button class="btn btn-warning btn-menu" data-toggle="modal" data-target="#myModal">Assign New Task</button>
-                </div>
-              <?php } ?>
-
-              </div>
-
-            </div>
-
-            
-          </div>
-          <center ><h3>Task Management Section</h3></center>
-          <div class="gap"></div>
-
-          <div class="gap"></div>
-
-          <div class="table-responsive">
-            <table class="table table-codensed table-custom">
-              <thead>
-                <tr>
-                  <th>#</th>
-                  <th>Task Title</th>
-                  <th>Assigned To</th>
-                  <th>Start Time</th>
-                  <th>End Time</th>
-                  <th>Status</th>
-                  <th>Action</th>
-                </tr>
-              </thead>
-              <tbody>
-
-              <?php 
-                if($user_role == 1){
-                  $sql = "SELECT a.*, b.fullname 
-                        FROM task_info a
-                        INNER JOIN tbl_admin b ON(a.t_user_id = b.user_id)
-                        ORDER BY a.task_id DESC";
-                }else{
-                  $sql = "SELECT a.*, b.fullname 
-                  FROM task_info a
-                  INNER JOIN tbl_admin b ON(a.t_user_id = b.user_id)
-                  WHERE a.t_user_id = $user_id
-                  ORDER BY a.task_id DESC";
-                } 
-                
-                  $info = $obj_admin->manage_all_info($sql);
-                  $serial  = 1;
-                  $num_row = $info->rowCount();
-                  if($num_row==0){
-                    echo '<tr><td colspan="7">No Data found</td></tr>';
-                  }
-                      while( $row = $info->fetch(PDO::FETCH_ASSOC) ){
-              ?>
-                <tr>
-                  <td><?php echo $serial; $serial++; ?></td>
-                  <td><?php echo $row['t_title']; ?></td>
-                  <td><?php echo $row['fullname']; ?></td>
-                  <td><?php echo $row['t_start_time']; ?></td>
-                  <td><?php echo $row['t_end_time']; ?></td>
-                  <td>
-                    <?php  if($row['status'] == 1){
-                        echo "In Progress <span style='color:#d4ab3a;' class=' glyphicon glyphicon-refresh' >";
-                    }elseif($row['status'] == 2){
-                       echo "Completed <span style='color:#00af16;' class=' glyphicon glyphicon-ok' >";
-                    }else{
-                      echo "Incomplete <span style='color:#d00909;' class=' glyphicon glyphicon-remove' >";
-                    } ?>
-                    
-                  </td>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/remixicon@4.2.0/fonts/remixicon.css">
   
-                 <td><a title="Update Task"  href="edit-task.php?task_id=<?php echo $row['task_id'];?>"><span class="glyphicon glyphicon-edit"></span></a>&nbsp;&nbsp;
-                  <a title="View" href="task-details.php?task_id=<?php echo $row['task_id']; ?>"><span class="glyphicon glyphicon-folder-open"></span></a>&nbsp;&nbsp;
-                  <?php if($user_role == 1){ ?>
-                  <a title="Delete" href="?delete_task=delete_task&task_id=<?php echo $row['task_id']; ?>" onclick=" return check_delete();"><span class="glyphicon glyphicon-trash"></span></a></td>
-                <?php } ?>
-                </tr>
-                <?php } ?>
-                
-              </tbody>
-            </table>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
+</head>
+
+<body>
+
+  <div class="page">
+    <?php
+    $page_name="Task_Info";
+    include("include/sidebar.php");
+    // include('ems_header.php');
+    ?>
+
+    <div class="content">
+      <!-- Modal -->
+      <div class="modal fade" id="myModal" role="dialog">
+        <div class="modal-dialog add-category-modal">
+        
+          <!-- Modal content-->
+          <div class="modal-content">
+            <div class="modal-header">
+              <button type="button" class="close" data-dismiss="modal">&times;</button>
+              <h2 class="modal-title text-center">Assign New Task</h2>
+            </div>
+            <div class="modal-body">
+              <div class="row">
+                <div class="col-md-12">
+                  <form role="form" action="" method="post" autocomplete="off">
+                    <div class="form-horizontal">
+                      <div class="form-group">
+                        <label class="control-label col-sm-5">Task Title</label>
+                        <div class="col-sm-7">
+                          <input type="text" placeholder="Task Title" id="task_title" name="task_title" list="expense" class="form-control" id="default" required>
+                        </div>
+                      </div>
+                      <div class="form-group">
+                        <label class="control-label col-sm-5">Task Description</label>
+                        <div class="col-sm-7">
+                          <textarea name="task_description" id="task_description" placeholder="Text Deskcription" class="form-control" rows="5" cols="5"></textarea>
+                        </div>
+                      </div>
+                      <div class="form-group">
+                        <label class="control-label col-sm-5">Start Time</label>
+                        <div class="col-sm-7">
+                          <input type="text" name="t_start_time" id="t_start_time" class="form-control">
+                        </div>
+                      </div>
+                      <div class="form-group">
+                        <label class="control-label col-sm-5">End Time</label>
+                        <div class="col-sm-7">
+                          <input type="text" name="t_end_time" id="t_end_time" class="form-control">
+                        </div>
+                      </div>
+                      <div class="form-group">
+                        <label class="control-label col-sm-5">Assign To</label>
+                        <div class="col-sm-7">
+                          <?php 
+                            $sql = "SELECT user_id, fullname FROM tbl_admin WHERE user_role = 2";
+                            $info = $obj_admin->manage_all_info($sql);   
+                          ?>
+                          <select class="form-control" name="assign_to" id="aassign_to" required>
+                            <option value="">Select Employee...</option>
+    
+                            <?php while($row = $info->fetch(PDO::FETCH_ASSOC)){ ?>
+                            <option value="<?php echo $row['user_id']; ?>"><?php echo $row['fullname']; ?></option>
+                            <?php } ?>
+                          </select>
+                        </div>
+                       
+                      </div>
+                      <div class="form-group">
+                      </div>
+                      <div class="form-group">
+                        <div class="col-sm-offset-3 col-sm-3">
+                          <button type="submit" name="add_task_post" class="btn btn-success-custom">Assign Task</button>
+                        </div>
+                        <div class="col-sm-3">
+                          <button type="submit" class="btn btn-danger-custom" data-dismiss="modal">Cancel</button>
+                        </div>
+                      </div>
+                    </div>
+                  </form> 
+                </div>
+              </div>
+    
+            </div>
           </div>
         </div>
       </div>
+        <div class="row">
+          <div class="col-md-12">
+            <div class="well well-custom">
+              <div class="gap"></div>
+              <div class="row">
+                <div class="col-md-8">
+                  <div class="btn-group">
+                    <?php if($user_role == 1){ ?>
+                    <div class="btn-group">
+                      <button class="btn btn-warning btn-menu" data-toggle="modal" data-target="#myModal">Assign New Task</button>
+                    </div>
+                  <?php } ?>
+    
+                  </div>
+    
+                </div>
+    
+                
+              </div>
+              <center ><h3>Task Management Section</h3></center>
+              <div class="gap"></div>
+    
+              <div class="gap"></div>
+    
+              <div class="table-responsive">
+                <table class="table table-codensed table-custom">
+                  <thead>
+                    <tr>
+                      <th>#</th>
+                      <th>Task Title</th>
+                      <th>Assigned To</th>
+                      <th>Start Time</th>
+                      <th>End Time</th>
+                      <th>Status</th>
+                      <th>Action</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+    
+                  <?php 
+                    if($user_role == 1){
+                      $sql = "SELECT a.*, b.fullname 
+                            FROM task_info a
+                            INNER JOIN tbl_admin b ON(a.t_user_id = b.user_id)
+                            ORDER BY a.task_id DESC";
+                    }else{
+                      $sql = "SELECT a.*, b.fullname 
+                      FROM task_info a
+                      INNER JOIN tbl_admin b ON(a.t_user_id = b.user_id)
+                      WHERE a.t_user_id = $user_id
+                      ORDER BY a.task_id DESC";
+                    } 
+                    
+                      $info = $obj_admin->manage_all_info($sql);
+                      $serial  = 1;
+                      $num_row = $info->rowCount();
+                      if($num_row==0){
+                        echo '<tr><td colspan="7">No Data found</td></tr>';
+                      }
+                          while( $row = $info->fetch(PDO::FETCH_ASSOC) ){
+                  ?>
+                    <tr>
+                      <td><?php echo $serial; $serial++; ?></td>
+                      <td><?php echo $row['t_title']; ?></td>
+                      <td><?php echo $row['fullname']; ?></td>
+                      <td><?php echo $row['t_start_time']; ?></td>
+                      <td><?php echo $row['t_end_time']; ?></td>
+                      <td>
+                        <?php  if($row['status'] == 1){
+                            echo "In Progress <span style='color:#d4ab3a;' class=' glyphicon glyphicon-refresh' >";
+                        }elseif($row['status'] == 2){
+                           echo "Completed <span style='color:#00af16;' class=' glyphicon glyphicon-ok' >";
+                        }else{
+                          echo "Incomplete <span style='color:#d00909;' class=' glyphicon glyphicon-remove' >";
+                        } ?>
+                        
+                      </td>
+      
+                     <td><a title="Update Task"  href="edit-task.php?task_id=<?php echo $row['task_id'];?>"><span class="glyphicon glyphicon-edit"></span></a>&nbsp;&nbsp;
+                      <a title="View" href="task-details.php?task_id=<?php echo $row['task_id']; ?>"><span class="glyphicon glyphicon-folder-open"></span></a>&nbsp;&nbsp;
+                      <?php if($user_role == 1){ ?>
+                      <a title="Delete" href="?delete_task=delete_task&task_id=<?php echo $row['task_id']; ?>" onclick=" return check_delete();"><span class="glyphicon glyphicon-trash"></span></a></td>
+                    <?php } ?>
+                    </tr>
+                    <?php } ?>
+                    
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
     </div>
-
-
+    
+  </div>
+  <!-- include the sidebar -->
 <?php
-
 include("include/footer.php");
-
-
-
 ?>
+</body>
+
 
 <script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>
 
@@ -229,3 +253,5 @@ include("include/footer.php");
   });
 
 </script>
+
+</html>
