8e453b2e36f02044e12e8c2a61c5b6bde333af18
Joshua <Joshua25@gmail.com>
2024-04-08 11:10:52 +0800

    Manage Admin and Intern

--- admin-manage-user.php
+++ admin-manage-user.php
@@ -33,7 +33,6 @@ if (isset($_GET['delete_user'])) {
 }
 
 $page_name = "Admin";
-include("include/sidebar.php");
 include("include/lib_links.php");
 
 if (isset($_POST['add_new_employee'])) {
@@ -44,53 +43,42 @@ if (isset($_POST['add_new_employee'])) {
 
 ?>
 
-
-
-
+<head>
+    <title>Admin Management | TaskPlanner</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
 
 <!--modal for employee add-->
 <!-- Modal -->
-<div class="modal fade" id="myModal" role="dialog">
-  <div class="modal-dialog">
 
+<form role="form" action="" enctype="multipart/form-data" method="post" autocomplete="off">
+  <?php if (isset($error)) { ?>
+                <h5 class="alert alert-danger"><?php echo $error; ?></h5>
+              <?php } ?>
+  <div id="modalBG">
+    <div class="modal">
+                <div class="modalTitle">
+                    <h2>Edit Intern</h2>
+                </div>
 
-    <!-- Modal content-->
-    <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal">&times;</button>
-        <h2 class="modal-title text-center">Add Employee Info</h2>
-      </div>
-      <div class="modal-body">
-        <div class="row">
-          <div class="col-md-12">
-            <?php if (isset($error)) { ?>
-              <h5 class="alert alert-danger"><?php echo $error; ?></h5>
-            <?php } ?>
-            <form role="form" action="" enctype="multipart/form-data" method="post" autocomplete="off">
-
-              <div class="form-horizontal">
-
-                <div class="form-group">
+                <div class="v-wrapper">
                   <label class="control-label col-sm-4">Fullname</label>
-                  <div class="col-sm-6">
-                    <input type="text" placeholder="Enter Employee Name" name="em_fullname" list="expense" class="form-control input-custom" id="default" required>
-                  </div>
-                </div>
-                <div class="form-group">
-                  <label class="control-label col-sm-4">Username</label>
-                  <div class="col-sm-6">
-                    <input type="text" placeholder="Enter Employee username" name="em_username" class="form-control input-custom" required>
-                  </div>
-                </div>
-                <div class="form-group">
-                  <label class="control-label col-sm-4">Email</label>
-                  <div class="col-sm-6">
-                    <input type="email" placeholder="Enter Employee Email" name="em_email" class="form-control input-custom" required>
-                  </div>
+                  <input type="text" placeholder="Enter Employee Name" name="em_fullname" list="expense" class="form-control" id="default" required>
                 </div>
 
-                <div class="form-group">
-                  <label class="control-label col-sm-4">Position</label>
+                <div class="v-wrapper">
+                  <label class="control-label col-sm-4">Username</label>
+                  <input type="text" placeholder="Enter Employee username" name="em_username" class="form-control" required>
+                </div>
+
+                <div class="v-wrapper">
+                  <label>Email</label>
+                    <input type="email" placeholder="Enter Employee Email" name="em_email" class="form-control" required>
+                </div>
+
+                <div class="v-wrapper">
+                  <label>Position</label>
                   <div class="col-sm-6">
                     <select name="position" class="form-control input-custom" required>
                       <option value="">Select Position...</option>
@@ -102,65 +90,50 @@ if (isset($_POST['add_new_employee'])) {
                   </div>
                 </div>
 
-                <div class="form-group">
-                  <label class="control-label col-sm-4">Profile Image</label>
-                  <div class="col-sm-6">
-                    <input type="file" name="profileimg" class="form-control input-custom">
-                  </div>
+                <div class="v-wrapper">
+                  <label>Profile Image</label>
+                  <input type="file" name="profileimg" class="form-control">
                 </div>
 
-
-
-
-
-                <div class="form-group">
-                </div>
-                <div class="form-group">
-                  <div class="col-sm-offset-3 col-sm-3">
+                <div class="btnSection">
                     <button type="submit" name="add_new_employee" class="btn btn-success-custom">Add Employee</button>
-                  </div>
-                  <div class="col-sm-3">
-                    <button type="submit" class="btn btn-danger-custom" data-dismiss="modal">Cancel</button>
-                  </div>
+                    <button id="exitModal">Cancel</button>
                 </div>
               </div>
-            </form>
-          </div>
-        </div>
-
-      </div>
-    </div>
-  </div>
-</div>
-
+              </div>
+    </form>
 
 
 <!--modal for employee add-->
 
 
-
-<div class="row">
-  <div class="col-md-12">
-    <div class="row">
-
-      <div class="well well-custom">
+<div class="page">
+        <?php
+        $page_name = "Admin";
+        include("include/sidebar.php");
+        // include('ems_header.php');
+        ?>
+<div class="content">
         <?php if (isset($error)) { ?>
           <script type="text/javascript">
-            $('#myModal').modal('show');
+            $('#modalBG').modal('show');
           </script>
         <?php } ?>
         <?php if ($user_role == 1) { ?>
           <div class="btn-group">
-            <button class="btn btn-success btn-menu" data-toggle="modal" data-target="#myModal">Add New Employee</button>
+            <button  id="openModal">Add New Employee</button>
           </div>
         <?php } ?>
-        <ul class="nav nav-tabs nav-justified nav-tabs-custom">
+
+
+        <!-- <ul class="nav nav-tabs nav-justified nav-tabs-custom"> -->
           <!-- <li><a href="manage-admin.php">Manage Admin</a></li> -->
           <!-- <li class="active"><a href="admin-manage-user.php">Manage Employee</a></li> -->
-        </ul>
-        <div class="gap"></div>
-        <div class="table-responsive">
-          <table class="table table-codensed table-custom">
+        <!-- </ul> -->
+
+        <div class="card">
+          <div class="table-container">
+          <table>
             <thead>
               <tr>
                 <th>Serial No.</th>
@@ -195,8 +168,19 @@ if (isset($_POST['add_new_employee'])) {
                   <td><?php echo $row['position']; ?></td>
                   <td><?php echo $row['temp_password']; ?></td>
 
-                  <td><a title="Update Employee" href="update-employee.php?admin_id=<?php echo $row['user_id']; ?>"><span class="glyphicon glyphicon-edit"></span></a>&nbsp;&nbsp;<a title="Delete" href="?delete_user=delete_user&admin_id=<?php echo $row['user_id']; ?>" onclick=" return check_delete();"><span class="glyphicon glyphicon-trash"></span></a></td>
+                  <td>
+                    <div class="actions">
+
+                      <a title="Update Employee" href="update-employee.php?admin_id=<?php echo $row['user_id']; ?>">
+                      <i class="ri-folder-open-fill"></i></a>&nbsp;&nbsp;<a title="Delete" href="?delete_user=delete_user&admin_id=<?php echo $row['user_id']; ?>" onclick=" return check_delete();">
+                      <i class="ri-delete-bin-6-fill"></i></a>
+                      
+                    </div>
+                  </td>
                 </tr>
+
+
+        
 
               <?php  } ?>
 
@@ -208,7 +192,6 @@ if (isset($_POST['add_new_employee'])) {
       </div>
     </div>
   </div>
-
 
   <?php
   if (isset($_SESSION['update_user_pass'])) {
